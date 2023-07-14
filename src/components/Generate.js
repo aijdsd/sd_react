ce9300f0bc36be644b45b1a0d829456336ccbd02
aijdsd <aijewellerydesign@gmail.com>
2023-07-14 17:19:54 +0530

    centered on generate, landing.***

--- src/components/Generate.js
+++ src/components/Generate.js
@@ -3,11 +3,13 @@ import { NavLink } from "react-router-dom";
 
 const Generate = () => {
     return(
-        <div className="d-block row align-items-center" style={{minHeight: "100vh"}}>
+        <div className="d-block " style={{minHeight: "100vh"}}>
+            <div className="d-flex" style={{minHeight: "40vh"}}><div className="vr"></div></div>
+            <center>
             <p>To Generate images click here</p>
             <button type="button" class="btn btn-outline-success"><NavLink to="/display">
                 Generate
-            </NavLink></button>
+            </NavLink></button></center>
         </div>
     )
 }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,11 +2,11 @@ import React from "react";
 const Landing = () => {
     return(
         <div className="d-block" style={{minHeight: "100vh"}}>
-            <div className="row align-items-center bg-success text-light" style={{minHeight: "100vh"}}>
+            <div className="d-flex" style={{minHeight: "40vh"}}><div className="vr"></div></div>
             <h1 className="text-center align-middle">Ai Jewellery Design</h1>
             <p className="text-center">This is a website to host Ai generated images of Jewellery for reference purposes.</p>
             <p className="text-center">Feel free to explore.</p>
-        </div></div>
+        </div>
     )
 }
 
