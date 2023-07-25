f140bbbcadfcba1038a47837326987a7c2b5ff80
aijdsd <aijewellerydesign@gmail.com>
2023-07-25 16:24:55 +0530

    about page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {  BrowserRouter, Route,  Routes } from 'react-router-dom';
 import Display from './pages/Display';
 import Landing from './components/Landing';
 import Home from './pages/Home';
+import About from './pages/about';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/display' element={<Display />}></Route>
+          <Route path='/about' element={<About />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+    return(
+        <div className="container-fluid" style={{ minHeight: "100vh" , alignContent: "center"}}>
+            <div className="d-flex" style={{minHeight: "40vh"}}><div className="vr"></div></div>
+            <center>
+            <h3>This site was made by:</h3>
+            <h1>Siddhartha Maity          Bharath Chandra</h1>
+            <p>This site was made in an effort to impove and grow ourselves.</p>
+            <p>It is not an amazing website , we know , but still is a fruit of our efforts.</p></center>
+        </div>
+    )
+}
+
+export default About;
