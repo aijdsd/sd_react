7c194ff85e0902d9ecbd8a5b9b2b861574edcf18
aijdsd <aijewellerydesign@gmail.com>
2023-07-25 13:26:07 +0530

    rangen flist -> smth

--- src/components/rangen.js
+++ src/components/rangen.js
@@ -3,17 +3,17 @@ import flist from "./flist";
 
 const Ran = () => {
     const valz =[]
-
+    const smth = flist 
     for (let index = 0; index < 12; index++) {
-        valz.push(Math.floor(Math.random() * flist.length));
+        valz.push(Math.floor(Math.random() * smth.length));
     }
 
     return (
             <ImageList cols={3} >
                 {valz.map((pos) => (
-                    <ImageListItem key={flist[pos].id}>
+                    <ImageListItem key={smth[pos].id}>
                     <img className='img-fluid'
-                        src={"https://drive.google.com/uc?id="+flist[pos].id}
+                        src={"https://drive.google.com/uc?id="+smth[pos].id}
                         alt="aijd"
                         loading="lazy"
                     />
